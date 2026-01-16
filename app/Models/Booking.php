1f85d8e84f0b4408cb1f313d7c2730cb6d9994bf
AlbertWebs <albertmuhatia@gmail.com>
2026-01-16 16:28:35 +0300

    update

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'location',
+        'apartment',
+        'message',
+        'test_slug',
+        'test_title',
+        'test_category',
+    ];
 }
